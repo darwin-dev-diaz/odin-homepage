04fa14364f0de7fd25f45a548558b30aa510ce17
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-10-07 10:52:48 -0400

    Finish project cards

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -6,6 +6,12 @@ function Project(props) {
     <div className={styles.project}>
       <div className={styles.project_image}>
         <img src={props.screenshot} alt="Software screenshot" />
+        <a href={props.liveLink} className={styles.external_link_icon}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <title>open-in-new</title>
+            <path d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" />
+          </svg>
+        </a>
       </div>
       <div className={styles.project_info}>
         <div className={styles.project_info_top}>
